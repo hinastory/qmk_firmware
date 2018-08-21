c40cee2a7a0be0f7c58b7116a8d2fe2b086c0953
hinastory <1696779+hinastory@users.noreply.github.com>
2018-08-22 07:00:09 +0900

    hinastory keymap for mint60

---
+++ keyboards/mint60/keymaps/hinastory/config.h
@@ -0,0 +1,32 @@
+/* Copyright 2018 Eucalyn
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+/* Use I2C or Serial, not both */
+#define USE_SERIAL
+// #define USE_I2C
+
+
+#define AUTO_SHIFT_TIMEOUT 150
+#define NO_AUTO_SHIFT_ALPHA
+
+#undef  TAPPING_TERM
+#define TAPPING_TERM 150
+#define IGNORE_MOD_TAP_INTERRUPT
+//#define RETRO_TAPPING
+
+// #define MASTER_RIGHT
