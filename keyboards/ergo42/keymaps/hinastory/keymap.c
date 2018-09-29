135431e5f8927d0fe93555f0cde2035568d6754e
hinastory <1696779+hinastory@users.noreply.github.com>
2018-09-29 09:36:43 +0900

    modify keymap

--- keyboards/ergo42/keymaps/hinastory/keymap.c
+++ keyboards/ergo42/keymaps/hinastory/keymap.c
@@ -11,6 +11,7 @@ extern keymap_config_t keymap_config;
 #define XXXXXXX KC_NO
 
 #define KC_MSPC LT(MO(META),KC_SPC)
+#define  KC_SBSP SFT_T(KC_BSPC)
 #define KC_SYMB MO(SYMBOL)
 #define KC_ADJ MO(ADJUST)
 
@@ -31,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,   KC_E,   KC_R,   KC_T, KC_LBRC,  KC_RBRC,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_MINS, \
     KC_LCTL, KC_A,    KC_S,   KC_D,   KC_F,   KC_G,KC_LANG2, KC_LANG1,  KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT, \
     KC_LALT, KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,  KC_EQL,  KC_GRV,  KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_BSLS, \
-    KC_LGUI, KC_ESC,KC_INS, KC_ADJ, KC_SYMB,KC_LSFT,KC_MSPC,  KC_BSPC,KC_ENT, KC_LGUI, KC_LCTL, KC_RSFT, KC_DEL, KC_HYPR \
+    KC_LGUI, KC_ESC,KC_INS, KC_ADJ, KC_SYMB,KC_LSFT,KC_MSPC,  KC_SBSP,KC_ENT, KC_LGUI, KC_LCTL, KC_RSFT, KC_DEL, KC_HYPR \
   ),
 
   /* META
